ad4b9abeec795de4d38eceb30e473063b04af651
desislavaandonova <161855212+desislavaandonova@users.noreply.github.com>
2024-04-04 20:54:52 +0200

    bullets added
    bulllet mechanism

--- NFCtag/DuckHuntGameView.swift
+++ NFCtag/DuckHuntGameView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
                 Spacer()
             }
             .navigationTitle("Duck Hunt")
+            .background(Color(UIColor.systemTeal).ignoresSafeArea())
         }
     }
 }
@@ -24,8 +25,9 @@ struct ContentView: View {
 struct DuckHuntGameView: View {
     @State private var ducks = [Duck]()
     @State private var gameIsOver = false
-    @State private var timeRemaining = 10
+    @State private var timeRemaining = 10 // 10 seconds for testing
     @State private var score = 0
+    @State private var bullets = 3 // maximum number of bullets
     
     var body: some View {
         Group {
@@ -34,91 +36,118 @@ struct DuckHuntGameView: View {
             } else {
                 VStack {
                     ZStack {
-                        Color(UIColor.systemTeal)
-                        ForEach(ducks) { duck in
-                            if !gameIsOver {
-                                DuckView(duck: duck)
-                                    .onTapGesture {
-                                        if let index = ducks.firstIndex(where: { $0.id == duck.id }) {
-                                            score += 10
-                                            ducks.remove(at: index)
-                                            if ducks.isEmpty {
-                                                endGame()
+                        ZStack {
+                            Color(UIColor.systemTeal)
+                            ForEach(ducks) { duck in
+                                if !gameIsOver {
+                                    DuckView(duck: duck)
+                                        .onTapGesture {
+                                            if bullets > 0 { // check if there are bullets left
+                                                bullets -= 1 // consume one bullet
+                                                
+                                                if let index = ducks.firstIndex(where: { $0.id == duck.id }) {
+                                                    score += 10
+                                                    ducks.remove(at: index)
+                                                    if ducks.isEmpty {
+                                                        endGame()
+                                                    }
+                                                }
                                             }
                                         }
-                                    }
+                                }
+                            }
+                        }
+                        
+                        VStack {
+                            Spacer()
+                            
+                            HStack {
+                                Spacer()
+                                Text("\(timeRemaining) seconds left")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .padding(10)
+                                    .background(Color.black.opacity(0.5))
+                                    .cornerRadius(10)
+                                
+                                Spacer()
+                                Text("Score: \(score)")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .padding(10)
+                                    .background(Color.black.opacity(0.5))
+                                    .cornerRadius(10)
+                                
+                                Spacer()
+
+                                Text("Bullets: \(bullets)")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .padding(10)
+                                    .background(Color.black.opacity(0.5))
+                                    .cornerRadius(10)
+                                
+                                Spacer()
+                            }
+                            .padding()
+                        }
+                    }
+                    .edgesIgnoringSafeArea(.top)
+                    .onAppear(perform: startGame)
+                    .onReceive(timer) { _ in
+                        if timeRemaining > 0 {
+                            timeRemaining -= 1
+                            if timeRemaining == 0 {
+                                endGame()
                             }
                         }
                     }
-                }
-                .navigationBarItems(
-                    leading:
-                        HStack {
-                            Text("\(timeRemaining) seconds left")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .padding(10)
-                                .background(Color.black.opacity(0.5))
-                                .cornerRadius(10)
-                                .padding()
-                        },
-                    trailing:
-                        HStack {
-                            Text("Score: \(score)")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .padding(10)
-                                .background(Color.black.opacity(0.5))
-                                .cornerRadius(10)
-                                .padding()
-                        }
-                )
-                .edgesIgnoringSafeArea(.top)
-                .onAppear(perform: startGame)
-                .onReceive(timer) { _ in
-                    if timeRemaining > 0 {
-                        timeRemaining -= 1
-                        if timeRemaining == 0 {
-                            endGame()
-                        }
-                    }
+                    .background(Color(UIColor.systemTeal).ignoresSafeArea())
                 }
             }
         }
     }
-
+    
+    // start the game by spawning ducks
     func startGame() {
-        ducks.removeAll()
+        ducks.removeAll() // clear leftoover ducks from previous game
+        bullets = 3 // reset bullets count
+        // generate 12 ducks
         for _ in 0..<12 {
             spawnDuck()
         }
     }
- 
+    
+    // spawn duck at a random position
     func spawnDuck() {
         let duck = Duck(position: randomPosition())
         ducks.append(duck)
         
+        // move the duck randomly
         withAnimation(Animation.linear(duration: 3).repeatForever(autoreverses: true)) {
             ducks[ducks.count - 1].position = randomPosition()
         }
     }
     
+    // generate a random position within the screen
     func randomPosition() -> CGPoint {
         return CGPoint(x: CGFloat.random(in: 0..<UIScreen.main.bounds.width),
                        y: CGFloat.random(in: 0..<UIScreen.main.bounds.height))
     }
     
+    // timer for duration
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
+    // end the game
     func endGame() {
         gameIsOver = true
     }
     
+    // func to replay the game
     func replayGame() {
         gameIsOver = false
         timeRemaining = 10
         score = 0
-        ducks.removeAll()
         startGame()
     }
 }
